80baf093605c039a656e645cc6ec581168e63d94
shinydev618 <shinydev618@gmail.com>
2023-06-23 03:39:13 -0400

    completd footer

---
+++ src/data/Link.js
@@ -0,0 +1,12 @@
+import { FaTelegramPlane, FaTwitter } from "react-icons/fa";
+
+export const dataSocialLink = [
+  {
+    icon: <FaTelegramPlane />,
+    linkUrl: "https://t.me/flappybee",
+  },
+  {
+    icon: <FaTwitter />,
+    linkUrl: "https://twitter.com/flappybee",
+  },
+];
--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -1,10 +1,29 @@
 import styled from "styled-components";
 import { Box } from "@mui/material";
+import { dataSocialLink } from "../../data/Link";
 
 const Footer = () => {
   return (
     <StyledComponent>
       <TextTitle>Flappy Bee</TextTitle>
+      <SectionLink>
+        <TextUpdate>Stay tuned for more updates</TextUpdate>
+        <SectionLinkButtonGroup>
+          {dataSocialLink?.map((each, index) => {
+            return (
+              <a
+                href={each.linkUrl}
+                key={index}
+                style={{ textDecoration: "none" }}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <ButtonSocialLink>{each.icon}</ButtonSocialLink>
+              </a>
+            );
+          })}
+        </SectionLinkButtonGroup>
+      </SectionLink>
     </StyledComponent>
   );
 };
@@ -26,6 +45,43 @@ const TextTitle = styled(Box)`
   line-height: 94.459px;
   -webkit-text-stroke: 3px #003d28;
   text-shadow: 5px 5px 0px rgb(0 115 75);
+`;
+
+const SectionLink = styled(Box)`
+  display: flex;
+  align-items: center;
+`;
+
+const TextUpdate = styled(Box)`
+  color: #fff;
+  font-size: 4em;
+  font-family: Rowdies;
+  line-height: 60px;
+  margin-right: 35px;
+`;
+
+const SectionLinkButtonGroup = styled(Box)`
+  display: flex;
+  align-items: center;
+`;
+
+const ButtonSocialLink = styled(Box)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 70px;
+  aspect-ratio: 1;
+  border-radius: 100%;
+  background-color: white;
+  color: #003d28;
+  cursor: pointer;
+  user-select: none;
+  font-size: 2.5rem;
+  margin-left: 20px;
+  transition: 0.3s;
+  &:hover {
+    color: #a9d100;
+  }
 `;
 
 export default Footer;
