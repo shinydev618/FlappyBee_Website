6e74acc4871e77dfdb4016ee411150cfafbce4fc
shinydev618 <shinydev618@gmail.com>
2023-09-05 13:55:32 -0400

    updated all

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
 
   function someRequest() {
     //Simulates a request; makes a "promise" that'll run for 2.5 seconds
-    return new Promise((resolve) => setTimeout(() => resolve(), 2500));
+    return new Promise((resolve) => setTimeout(() => resolve(), 1000));
   }
 
   useEffect(() => {
